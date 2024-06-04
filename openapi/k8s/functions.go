39c7b70f3e44e210bbb5b7e7cd78d091a8babd35
Ladislav Čapka <lcapka@gmail.com>
2024-06-04 12:56:47 +0200

    fix(openapi/k8s/functions): GetCurrentNamespace

--- openapi/k8s/functions.go
+++ openapi/k8s/functions.go
@@ -13,11 +13,9 @@ var (
 // GetCurrentNamespace returns the namespace of the pod in which the application is running. If the namespace is not found, it returns "default".
 func GetCurrentNamespace() (namespace string, loaded bool) {
 	if !_namespaceLoaded {
-		ns := os.Getenv("NAMESPACE")
+		ns := strings.TrimSpace(os.Getenv("NAMESPACE"))
 		if len(ns) == 0 {
 			ns = "default"
-		} else {
-			ns = strings.TrimSpace(ns)
 		}
 		_namespace = ns
 		_namespaceLoaded = true
