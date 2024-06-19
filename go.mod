d49ec90fd2101598107025d953a2660dbf811c21
Ladislav Čapka <lcapka@gmail.com>
2024-06-19 21:15:17 +0200

    refactor(pbauthentication.go): add UserInfo model

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ retract (
 )
 
 require (
+	github.com/cerbos/cerbos-sdk-go v0.2.7
 	github.com/getkin/kin-openapi v0.125.0
 	github.com/google/uuid v1.6.0
 	github.com/oapi-codegen/runtime v1.1.1
@@ -17,19 +18,49 @@ require (
 )
 
 require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.2-20240508200655-46a4cf4ba109.2 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/bufbuild/protovalidate-go v0.6.2 // indirect
+	github.com/cerbos/cerbos/api/genpb v0.37.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/cel-go v0.20.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.1.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	github.com/invopop/yaml v0.3.1 // indirect
+	github.com/jdxcode/netrc v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.5 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.1.0 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/planetscale/vtprotobuf v0.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240617180043-68d350f18fd4 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240617180043-68d350f18fd4 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- protobuf/pbauthentication/pbauthentication.go
+++ protobuf/pbauthentication/pbauthentication.go
@@ -3,39 +3,68 @@ package pbauthentication
 import (
 	"context"
 
+	"github.com/cerbos/cerbos-sdk-go/cerbos"
 	"google.golang.org/grpc/metadata"
 )
 
+// UserInfo represents the user information extracted during authentication.
+type UserInfo struct {
+	// OwnerId represents the subject's organization identifier.
+	OwnerId string
+	// Subject represents the user ID.
+	Subject string
+	// Issuer of the token.
+	Issuer string
+	// Roles represents the roles the user has.
+	Roles map[string]struct{}
+	// Exp represents the expiration time of the token.
+	Exp int64
+	// Raw represents the raw token data.
+	Raw map[string]interface{}
+	// Principal represents the principal of the user to handle authorization.
+	Principal *cerbos.Principal
+}
+
 // NewGrpcIncomingContext returns a new outgoing context with the subject and issuer in gRPC metadata.
-func NewOutgoingContext(subject string, issuer string) context.Context {
-	return NewOutgoingContextFromContext(context.Background(), subject, issuer)
+func NewOutgoingContext(userInfo *UserInfo) context.Context {
+	return NewOutgoingContextFromContext(context.Background(), userInfo)
 }
 
 // NewGrpcIncomingContextFromContext returns outgoing context derived from ctx with the subject and issuer in gRPC metadata.
-func NewOutgoingContextFromContext(ctx context.Context, subject string, issuer string) context.Context {
+func NewOutgoingContextFromContext(ctx context.Context, userInfo *UserInfo) context.Context {
 	md := metadata.MD{}
-	if len(subject) > 0 {
-		md.Append("io-clbs-openhes-auth-sub", subject)
-	}
-	if len(issuer) > 0 {
-		md.Append("io-clbs-openhes-auth-iss", issuer)
+	if userInfo != nil {
+		if len(userInfo.Subject) > 0 {
+			md.Append("io-clbs-openhes-auth-sub", userInfo.Subject)
+		}
+		if len(userInfo.Issuer) > 0 {
+			md.Append("io-clbs-openhes-auth-iss", userInfo.Issuer)
+		}
+		if len(userInfo.Issuer) > 0 {
+			md.Append("io-clbs-openhes-auth-ownerid", userInfo.OwnerId)
+		}
 	}
 	return metadata.NewOutgoingContext(ctx, md)
 }
 
 // GetGrpcAuthInfo returns the subject and issuer from incoming gRPC context.
-func FromIncomingContext(ctx context.Context) (subject string, issuer string) {
+func FromIncomingContext(ctx context.Context) (userInfo *UserInfo) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return "", ""
+		return nil
 	}
+	userInfo = &UserInfo{}
 	subjects := md.Get("io-clbs-openhes-auth-sub")
 	if len(subjects) > 0 {
-		subject = subjects[0]
+		userInfo.Subject = subjects[0]
 	}
 	issuers := md.Get("io-clbs-openhes-auth-iss")
 	if len(issuers) > 0 {
-		issuer = issuers[0]
+		userInfo.Issuer = issuers[0]
+	}
+	ownerIds := md.Get("io-clbs-openhes-auth-ownerid")
+	if len(ownerIds) > 0 {
+		userInfo.OwnerId = ownerIds[0]
 	}
 	return
 }
