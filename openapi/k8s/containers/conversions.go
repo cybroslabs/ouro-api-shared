4a9852ff75ac42f087225893fbee4f3f436bb266
Ladislav Čapka <lcapka@gmail.com>
2024-12-08 15:06:16 +0100

    feat(convertions.go): add more convertions

--- openapi/k8s/containers/conversions.go
+++ openapi/k8s/containers/conversions.go
@@ -12,19 +12,71 @@ import (
 )
 
 var (
+	ErrInvalidResourceLimitsValue = fmt.Errorf("invalid limits value, limits cannot be nil")
 	ErrInvalidResourceCpuValue    = fmt.Errorf("invalid cpu value")
 	ErrInvalidResourceMemoryValue = fmt.Errorf("invalid memory value")
 )
 
-// Converts the container resources - gRPC to REST
-func G2RContainerResources(in *ContainerResourcesSchema) (*pbtaskmaster.ContainerResources, error) {
+// Converts the container resources set - REST to gRPC
+func R2GContainerResourcesSet(in *ContainerResourcesSetSchema) (result *pbtaskmaster.ContainerResourceSet, err error) {
+	if in == nil {
+		return
+	}
+
+	var limits *pbtaskmaster.ContainerResources
+	var requests *pbtaskmaster.ContainerResources
+
+	if limits, err = R2GContainerResources(&in.Limits); err != nil {
+		return
+	}
+	if requests, err = R2GContainerResources(in.Requests); err != nil {
+		return
+	}
+
+	result = &pbtaskmaster.ContainerResourceSet{
+		Limits:   limits,
+		Requests: requests,
+	}
+
+	return
+}
+
+// Converts the container resources set - gRPC to REST
+func G2RContainerResourcesSet(in *pbtaskmaster.ContainerResourceSet) (result *ContainerResourcesSetSchema, err error) {
+	if in == nil {
+		return
+	}
+	if in.Limits == nil {
+		return nil, ErrInvalidResourceLimitsValue
+	}
+
+	var limits *ContainerResourcesSchema
+	var requests *ContainerResourcesSchema
+
+	if limits, err = G2RContainerResources(in.Limits); err != nil {
+		return
+	}
+	if requests, err = G2RContainerResources(in.Requests); err != nil {
+		return
+	}
+
+	result = &ContainerResourcesSetSchema{
+		Limits:   *limits,
+		Requests: requests,
+	}
+
+	return
+}
+
+// Converts the container resources - REST to gRPC
+func R2GContainerResources(in *ContainerResourcesSchema) (*pbtaskmaster.ContainerResources, error) {
 	if in == nil {
 		return nil, nil
 	}
 
 	var cpu string
-
 	var cpu_f ContainerResourcesCpuIntSchema
+
 	d := json.NewDecoder(bytes.NewReader(in.Cpu.union))
 	d.UseNumber()
 	err := d.Decode(&cpu_f)
@@ -61,4 +113,18 @@ func G2RContainerResources(in *ContainerResourcesSchema) (*pbtaskmaster.Containe
 		Cpu:    cpu,
 		Memory: in.Memory,
 	}, nil
+}
+
+// Converts the container resources - gRPC to REST
+func G2RContainerResources(in *pbtaskmaster.ContainerResources) (result *ContainerResourcesSchema, err error) {
+	if in == nil {
+		return
+	}
+
+	result = &ContainerResourcesSchema{
+		Memory: in.Memory,
+	}
+	err = result.Cpu.FromContainerResourcesCpuStrSchema(in.Cpu)
+
+	return
 }
