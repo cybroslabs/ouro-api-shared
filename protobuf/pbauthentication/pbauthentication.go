67c2f41c464d089ebbe41f099f8373eaa83ef3fa
Ladislav Čapka <lcapka@gmail.com>
2024-12-11 16:16:37 +0100

    fix(pbauthentication.go): support for extendable outgoing context data

--- protobuf/pbauthentication/pbauthentication.go
+++ protobuf/pbauthentication/pbauthentication.go
@@ -25,29 +25,29 @@ type UserInfo struct {
 	Principal *cerbos.Principal
 }
 
-// NewGrpcIncomingContext returns a new outgoing context with the subject and issuer in gRPC metadata.
-func NewOutgoingContext(userInfo *UserInfo) context.Context {
-	return NewOutgoingContextFromContext(context.Background(), userInfo)
-}
+// NewOutgoingContextUser returns outgoing context derived from ctx with the sub, iss and ownerid set. Original context values are preserved.
+func NewOutgoingContextUser(ctx context.Context, userInfo *UserInfo) context.Context {
+	if userInfo == nil {
+		return ctx
+	}
 
-// NewGrpcIncomingContextFromContext returns outgoing context derived from ctx with the subject and issuer in gRPC metadata.
-func NewOutgoingContextFromContext(ctx context.Context, userInfo *UserInfo) context.Context {
-	md := metadata.MD{}
-	if userInfo != nil {
-		if len(userInfo.Subject) > 0 {
-			md.Append("io-clbs-openhes-auth-sub", userInfo.Subject)
-		}
-		if len(userInfo.Issuer) > 0 {
-			md.Append("io-clbs-openhes-auth-iss", userInfo.Issuer)
-		}
-		if len(userInfo.Issuer) > 0 {
-			md.Append("io-clbs-openhes-auth-ownerid", userInfo.OwnerId)
-		}
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if !ok {
+		md = metadata.MD{}
+	}
+	if len(userInfo.Subject) > 0 {
+		md.Set("io-clbs-openhes-auth-sub", userInfo.Subject)
+	}
+	if len(userInfo.Issuer) > 0 {
+		md.Set("io-clbs-openhes-auth-iss", userInfo.Issuer)
+	}
+	if len(userInfo.Issuer) > 0 {
+		md.Set("io-clbs-openhes-auth-ownerid", userInfo.OwnerId)
 	}
 	return metadata.NewOutgoingContext(ctx, md)
 }
 
-// GetGrpcAuthInfo returns the subject and issuer from incoming gRPC context.
+// FromIncomingContext returns the subject and issuer from incoming gRPC context.
 func FromIncomingContext(ctx context.Context) (userInfo *UserInfo) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
