910c88c1ba1871ab7872b519ff876b6b8a4ca495
Ladislav Čapka <lcapka@gmail.com>
2024-12-11 09:54:14 +0100

    refactor(driverdescriptor.go): move and add helper methods

--- openapi/openhes/driver/driverdescriptor.go
+++
@@ -1,9 +0,0 @@
-package driver
-
-// DriverDescriptor is a struct to hold the driver descriptor. It is rendered out
-// when the driver is started with --descriptor=<format> argument.
-// The <format> should be json, but it's reserved for future use.
-type DriverDescriptor struct {
-	// DriverDescriptor is a base64 encoded value protobuf of the driver descriptor, see pbdriver.NegotiateRequest.
-	Descriptor string `json:"driver"`
-}
