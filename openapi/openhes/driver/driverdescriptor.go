0873e22173726d0ae99897a9c643222b8ee5a8be
Ladislav Čapka <lcapka@gmail.com>
2024-12-11 09:31:19 +0100

    refactor(driverdescriptor.go): json won't deserialize content

--- openapi/openhes/driver/driverdescriptor.go
+++ openapi/openhes/driver/driverdescriptor.go
@@ -1,10 +1,9 @@
 package driver
 
-import "github.com/cybroslabs/hes-2-apis/protobuf/pbdriver"
-
 // DriverDescriptor is a struct to hold the driver descriptor. It is rendered out
 // when the driver is started with --descriptor=<format> argument.
 // The <format> should be json, but it's reserved for future use.
 type DriverDescriptor struct {
-	Descriptor pbdriver.NegotiateRequest `json:"driver"`
+	// DriverDescriptor is a base64 encoded value protobuf of the driver descriptor, see pbdriver.NegotiateRequest.
+	Descriptor string `json:"driver"`
 }
