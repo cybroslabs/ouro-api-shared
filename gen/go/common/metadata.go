adae0c89e17c8c8ccca07f2f3afb2f75c00eaf1a
Ladislav Čapka <lcapka@gmail.com>
2025-01-24 22:53:30 +0100

    feat: add metadata helpers

---
+++ gen/go/common/metadata.go
@@ -0,0 +1,34 @@
+package common
+
+import "github.com/google/uuid"
+
+// Create creates a new Metadata object with a new UUID and generation 1.
+func (x *MetadataFields) Create() error {
+	if x == nil {
+		return nil
+	}
+	new_id, err := uuid.NewV7()
+	if err != nil {
+		return err
+	}
+	x.SetId(new_id.String())
+	x.SetGeneration(1)
+	return nil
+}
+
+// IncGeneration increments the generation of the object.
+func (x *MetadataFields) IncGeneration() {
+	x.SetGeneration(x.GetGeneration() + 1)
+}
+
+// Id returns the UUID of the object or nil if the object is nil or the ID is invalid.
+func (x *MetadataFields) Id() uuid.UUID {
+	if x == nil {
+		return uuid.Nil
+	}
+	id, err := uuid.Parse(x.GetId())
+	if err != nil {
+		return uuid.Nil
+	}
+	return id
+}
