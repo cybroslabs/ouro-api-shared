da76837db75fc532187db22398a2773c225f6388
Ladislav Čapka <lcapka@gmail.com>
2025-06-05 16:49:30 +0200

    refactor: simplify NewZapLogger

--- gen/go/helpers/logging/logging.go
+++ gen/go/helpers/logging/logging.go
@@ -1,29 +1,29 @@
 package logging
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 )
 
-// GetZapLogger returns a zap logger configured based on the provided parameters.
-// If inKubernetes is true, it uses production configuration; otherwise, it uses development configuration.
-// If logLevel is provided, it sets the logger's level accordingly.
-// The function returns a pointer to a zap.Logger instance.
-func NewZapLogger(inKubernetes bool, logLevel *string) *zap.Logger {
+// NewZapLogger returns a zap logger configured based on the provided parameters.
+// It checks if the application is running in a Kubernetes environment by looking for the KUBERNETES_SERVICE_HOST environment variable.
+// If it is running in Kubernetes, it uses the production configuration; otherwise, it uses the development configuration.
+// The logger is configured to use the log level specified by the LOG_LEVEL environment variable.
+// If the LOG_LEVEL is not set or is invalid, it defaults to the zap logger's default level.
+func NewZapLogger() *zap.Logger {
 	var zap_logger *zap.Logger
-	if inKubernetes {
+	var logLevel = os.Getenv("LOG_LEVEL")
+	if _, ok := os.LookupEnv("KUBERNETES_SERVICE_HOST"); ok {
 		zap_config := zap.NewProductionConfig()
-		if logLevel != nil {
-			if lvl, err := zap.ParseAtomicLevel(*logLevel); err == nil {
-				zap_config.Level = lvl
-			}
+		if lvl, err := zap.ParseAtomicLevel(logLevel); err == nil {
+			zap_config.Level = lvl
 		}
 		zap_logger, _ = zap_config.Build()
 	} else {
 		zap_config := zap.NewDevelopmentConfig()
-		if logLevel != nil {
-			if lvl, err := zap.ParseAtomicLevel(*logLevel); err == nil {
-				zap_config.Level = lvl
-			}
+		if lvl, err := zap.ParseAtomicLevel(logLevel); err == nil {
+			zap_config.Level = lvl
 		}
 		zap_logger, _ = zap_config.Build()
 	}
