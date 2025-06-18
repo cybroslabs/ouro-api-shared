db15a01cf20f4ed55c33ae242aaa984010601413
Ladislav Čapka <lcapka@gmail.com>
2025-06-18 14:35:27 +0200

    feat: add helper for licensing

---
+++ gen/go/helpers/licensemanager.go
@@ -0,0 +1,44 @@
+package helpers
+
+import (
+	"context"
+
+	"github.com/cybroslabs/ouro-api-shared/gen/go/system"
+)
+
+// LicenseManager is an interface that defines methods for license operations in the system.
+type LicenseManager interface {
+	// GetLicense retrieves the current license key.
+	GetLicense(ctx context.Context) (*system.License, error)
+}
+
+// The LicenseManagerOpts struct contains options for creating a LicenseManager instance.
+type LicenseManagerOpts struct {
+	Connectors Connectors
+}
+
+type licenseManager struct {
+	connectors Connectors
+}
+
+// NewLicenseManager creates a new instance of LicenseManager with the provided options.
+func NewLicenseManager(opts LicenseManagerOpts) LicenseManager {
+	return &licenseManager{
+		connectors: opts.Connectors,
+	}
+}
+
+// Returns the current license.
+func (lm *licenseManager) GetLicense(ctx context.Context) (*system.License, error) {
+	cli, closer, err := lm.connectors.OpenOuroOperatorServiceClient()
+	if err != nil {
+		return nil, err
+	}
+	defer closer()
+
+	resp, err := cli.GetLicense(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
