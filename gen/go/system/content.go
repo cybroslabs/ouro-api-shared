8e14c3b15cab74f0c84da616e47a40ec61f0d930
Ladislav Čapka <lcapka@gmail.com>
2025-06-19 00:10:53 +0200

    chore: change builtin content behavior

--- gen/go/system/content.go
+++ gen/go/system/content.go
@@ -2,7 +2,27 @@ package system
 
 import (
 	embed "embed"
+	"io"
+	"io/fs"
 )
 
 //go:embed content/license.pem
-var Content embed.FS
+var content embed.FS
+
+var subfolder fs.FS
+
+func init() {
+	subfolder, _ = fs.Sub(content, "content")
+}
+
+// ReadFile reads and returns the content of the named file.
+func ReadFile(path string) ([]byte, error) {
+	f, err := subfolder.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+
+	// Read the file content
+	return io.ReadAll(f)
+}
